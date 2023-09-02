8b1fd28adc5e89930f74fa7c154abfb66f165b2c
Flambrew <77941661+Flambrew@users.noreply.github.com>
2023-09-02 01:53:15 -0400

    created window class, reused old resolution enum

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -1,21 +1,26 @@
 public abstract class Engine extends Thread {
 	private double millisecondsPerFrame, lastFrameMillis;
 
+	private Window window;
+
 	public Engine(int framesPerSecond) {
 		this.millisecondsPerFrame = 1000. / framesPerSecond;
+		start();
 	}
 
 	public void run() {
-		lastFrameMillis = System.currentTimeMillis();
 		init();
+		lastFrameMillis = System.currentTimeMillis();
 
 		while (true) {
 			frameupdate();
 
 			if (System.currentTimeMillis() - lastFrameMillis > millisecondsPerFrame) {
-				lastFrameMillis += millisecondsPerFrame;
 				fixedupdate();
+				lastFrameMillis += millisecondsPerFrame;
 			}
+
+			window.repaint();
 		}
 	}
 
---
+++ src/engine/Window.java
@@ -0,0 +1,21 @@
+import javax.swing.JPanel;
+
+public class Window extends JPanel {
+	public enum Resolution {
+		nHD360(640, 360), SVGA600(800, 600), XGA768(1024, 768), WXGA720(1280, 720), WXGA800(1280, 800), SXGA1024(1280, 1024), HD768(1366, 768),
+		WXGAp900(1440, 900), HDp900(1600, 900), UXGA1200(1600, 1200), WSXGAp1050(1050, 1680), FHD1080(1920, 1080), WUXGA1200(1920, 1200), QWXGA1152(2048, 1152),
+		UWFHD1080(2560, 1080), QHD1440(2560, 1440), WQXGA1600(2560, 1600), UWQHD1440(3440, 1440), UHD4K2160(3840, 2160);
+
+		private int x, y;
+
+		private Resolution(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public int x() { return x; }
+
+		public int y() { return y; }
+	}
+	
+}
