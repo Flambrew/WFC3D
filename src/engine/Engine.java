e2b4a6f217877b1ccde55fc4f72ba758029030a4
Flambrew <77941661+Flambrew@users.noreply.github.com>
2023-09-02 01:09:46 -0400

    Engine frame- and fixed-update system created

--- src/engine/Engine.java
+++ src/engine/Engine.java
@@ -1,18 +1,31 @@
-public class Engine {
-	private int framerate;
-	private boolean initialized, running;
-	
-	
+public abstract class Engine extends Thread {
+	private int framesPerSecond, currentFrame;
+	private double millisecondsPerFrame, lastFrameMillis;
 
-	public boolean start() {
-		return initialized ? false : initialized = true;
+	public Engine(int framesPerSecond) {
+		this.framesPerSecond = framesPerSecond;
+		this.millisecondsPerFrame = 1000. / framesPerSecond;
 	}
 
-	public boolean resume() {
-		return !running ? false : running = true;
+	public void run() {
+		lastFrameMillis = System.currentTimeMillis();
+		init();
+
+		while (true) {
+			frameupdate();
+
+			if (System.currentTimeMillis() - lastFrameMillis > millisecondsPerFrame) {
+				lastFrameMillis += millisecondsPerFrame;
+				currentFrame += 1;
+
+				fixedupdate();
+			}
+		}
 	}
 
-	public boolean stop() {
-		return running ? false : !(running = true);
-	}
+	public abstract void init();
+
+	public abstract void frameupdate();
+
+	public abstract void fixedupdate();
 }
